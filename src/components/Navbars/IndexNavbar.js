732ac5deb71b661bd9d6a507848d210b12496037
Vamsi Vikash <vamsivikash@gmail.com>
2021-07-28 22:40:26 -0700

    Base Files

---
+++ src/components/Navbars/IndexNavbar.js
@@ -0,0 +1,211 @@
+/*!
+
+=========================================================
+* BLK Design System React - v1.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/blk-design-system-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/blk-design-system-react/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import { Link } from "react-router-dom";
+import Resume from '../../assets/Resume/Vamsi_Vikash_Ankam.pdf';
+// reactstrap components
+import {
+  Button,
+  Collapse,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  UncontrolledDropdown,
+  NavbarBrand,
+  Navbar,
+  NavItem,
+  NavLink,
+  Nav,
+  Container,
+  Row,
+  Col,
+  UncontrolledTooltip,
+} from "reactstrap";
+
+export default function IndexNavbar() {
+  const [collapseOpen, setCollapseOpen] = React.useState(false);
+  const [collapseOut, setCollapseOut] = React.useState("");
+  const [color, setColor] = React.useState("navbar-transparent");
+  React.useEffect(() => {
+    window.addEventListener("scroll", changeColor);
+    return function cleanup() {
+      window.removeEventListener("scroll", changeColor);
+    };
+  },[]);
+  const changeColor = () => {
+    if (
+      document.documentElement.scrollTop > 99 ||
+      document.body.scrollTop > 99
+    ) {
+      setColor("bg-info");
+    } else if (
+      document.documentElement.scrollTop < 100 ||
+      document.body.scrollTop < 100
+    ) {
+      setColor("navbar-transparent");
+    }
+  };
+  const toggleCollapse = () => {
+    document.documentElement.classList.toggle("nav-open");
+    setCollapseOpen(!collapseOpen);
+  };
+  const onCollapseExiting = () => {
+    setCollapseOut("collapsing-out");
+  };
+  const onCollapseExited = () => {
+    setCollapseOut("");
+  };
+  const scrollToDownload = () => {
+    return {
+      mime: 'text/pdf',
+      filename: '../../assets/Resume/Vamsi_Vikash_Ankam.pdf',
+      contents: 'Resume_VamsiVikash.pdf',
+     }
+
+    // document
+    //   .getElementById("download-section")
+    //   .scrollIntoView({ behavior: "smooth" });
+  };
+  return (
+    <Navbar className={"fixed-top " + color} color-on-scroll="100" expand="lg">
+      <Container>
+        <div className="navbar-translate">
+          <NavbarBrand to="/" tag={Link} id="navbar-brand">
+            <span> VAMSI VIKASH • </span>
+            Serverless Specialist
+          </NavbarBrand>
+          <UncontrolledTooltip placement="bottom" target="navbar-brand">
+            Technical Account Manager, Serverless Specialist, Solution Architect
+          </UncontrolledTooltip>
+          <button
+            aria-expanded={collapseOpen}
+            className="navbar-toggler navbar-toggler"
+            onClick={toggleCollapse}
+          >
+            <span className="navbar-toggler-bar bar1" />
+            <span className="navbar-toggler-bar bar2" />
+            <span className="navbar-toggler-bar bar3" />
+          </button>
+        </div>
+        <Collapse
+          className={"justify-content-end " + collapseOut}
+          navbar
+          isOpen={collapseOpen}
+          onExiting={onCollapseExiting}
+          onExited={onCollapseExited}
+        >
+          <div className="navbar-collapse-header">
+            <Row>
+              <Col className="collapse-brand" xs="6">
+                <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                  VAMSI VIKASH • Serverless Specialist
+                </a>
+              </Col>
+              <Col className="collapse-close text-right" xs="6">
+                <button
+                  aria-expanded={collapseOpen}
+                  className="navbar-toggler"
+                  onClick={toggleCollapse}
+                >
+                  <i className="tim-icons icon-simple-remove" />
+                </button>
+              </Col>
+            </Row>
+          </div>
+          <Nav navbar>
+            <NavItem className="p-0">
+              <NavLink
+                data-placement="bottom"
+                href="https://twitter.com/vamsi_vikash"
+                rel="noopener noreferrer"
+                target="_blank"
+                title="Follow me on Twitter"
+              >
+                <i className="fab fa-twitter" />
+                <p className="d-lg-none d-xl-none">Twitter</p>
+              </NavLink>
+            </NavItem>
+            <NavItem className="p-0">
+              <NavLink
+                data-placement="bottom"
+                href="https://www.linkedin.com/in/vamsivikash/"
+                rel="noopener noreferrer"
+                target="_blank"
+                title="Message me on Linkedin"
+              >
+                <i className="fab fa-linkedin-in" />
+                <p className="d-lg-none d-xl-none">Linkedin</p>
+              </NavLink>
+            </NavItem>
+            <NavItem className="p-0">
+              <NavLink
+                data-placement="bottom"
+                href="https://vamsi-vikash.medium.com"
+                rel="noopener noreferrer"
+                target="_blank"
+                title="Follow me on Medium"
+              >
+                <i className="fab fa-medium" />
+                <p className="d-lg-none d-xl-none">Medium</p>
+              </NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav>
+              <DropdownToggle
+                caret
+                color="default"
+                data-toggle="dropdown"
+                href="#pablo"
+                nav
+                onClick={(e) => e.preventDefault()}
+              >
+                <i className="fa fa-cogs d-lg-none d-xl-none" />
+                Explore
+              </DropdownToggle>
+              <DropdownMenu className="dropdown-with-icons">
+                <DropdownItem tag={Link} to="/register-page">
+                  <i className="tim-icons icon-bullet-list-67" />
+                  Register Page
+                </DropdownItem>
+                <DropdownItem tag={Link} to="/landing-page">
+                  <i className="tim-icons icon-image-02" />
+                  Blog
+                </DropdownItem>
+                <DropdownItem tag={Link} to="/profile-page">
+                  <i className="tim-icons icon-single-02" />
+                  Profile Page
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+            
+            <NavItem>
+              <Button
+                className="nav-link d-none d-lg-block"
+                color="default"
+                href={Resume}
+                download = "Resume_Vamsi_Vikash.pdf"
+                target="_blank"
+              >
+                <i className="tim-icons icon-cloud-download-93" /> Download Resume
+              </Button>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Container>
+    </Navbar>
+  );
+}
